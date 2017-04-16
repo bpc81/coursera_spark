ea94e7e3aa25abbdbc0aaa20bc8b92eaa1ab9783
Natalie Gryphon <gryphon.natalie@gmail.com>
2017-04-16 13:47:28 -0400

    setting column names in timeUsageSummary

--- timeusage/src/main/scala/timeusage/TimeUsage.scala
+++ timeusage/src/main/scala/timeusage/TimeUsage.scala
@@ -147,20 +147,25 @@ object TimeUsage {
     val workingStatusProjection: Column = when(
       df("telfs") >= 1 && df("telfs") < 3, "working")
         .otherwise("not working")
+      .as("working")
 
     val sexProjection: Column = when(
       df("tesex") === 1, "male")
       .otherwise("female")
+      .as("sex")
 
     val ageProjection: Column = when(
       df("teage") between(15,22), "young")
       .when(df("teage") between(23,55), "active")
       .otherwise("elder")
+      .as("age")
 
-
-    val primaryNeedsProjection: Column = primaryNeedsColumns.reduce(_ + _) / 60
-    val workProjection: Column = workColumns.reduce(_ + _) / 60
-    val otherProjection: Column = otherColumns.reduce(_ + _) / 60
+    val primaryNeedsProjection: Column = (primaryNeedsColumns.reduce(_ + _) / 60)
+      .as("primaryNeeds")
+    val workProjection: Column = (workColumns.reduce(_ + _) / 60)
+      .as("work")
+    val otherProjection: Column = (otherColumns.reduce(_ + _) / 60)
+      .as("other")
     df
       .select(workingStatusProjection, sexProjection, ageProjection, primaryNeedsProjection, workProjection, otherProjection)
       .where($"telfs" <= 4) // Discard people who are not in labor force
