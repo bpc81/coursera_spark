04465299542b624c5d897f2389258e8c3fd6a2fb
Natalie Gryphon <gryphon.natalie@gmail.com>
2017-04-16 13:15:27 -0400

    adding unit tests for reading csv file

--- timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
+++ timeusage/src/test/scala/timeusage/TimeUsageSuite.scala
@@ -15,7 +15,31 @@ import scala.util.Random
 
 @RunWith(classOf[JUnitRunner])
 class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+  import TimeUsage._
 
+  val resource = "/timeusage/atussum.csv"
 
+  test("reading data file schema") {
+    val rdd = spark.sparkContext.textFile(fsPath(resource))
+
+    val headerColumns = rdd.first().split(",").to[List]
+    // Compute the schema based on the first line of the CSV file
+    val schema = dfSchema(headerColumns)
+
+    println(schema)
+  }
+
+  test("reading data file") {
+    val (columns, initDf) = read(resource)
+    val numCols = columns.length
+    assert(numCols === 455)
+    println(s"Number of Columns: $numCols")
+    //println(columns.length)
+    //initDf.printSchema()
+    //initDf.show(10)
+    val numRows = initDf.count()
+    assert(numRows === 170842)
+    println(s"Number of rows: $numRows")
+  }
 
 }
